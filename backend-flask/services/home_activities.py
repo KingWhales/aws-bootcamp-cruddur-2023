c5f19f53f1064dc4b400101c5694764896d81c89
KingWhales <olawaleafosi@gmail.com>
2023-05-08 13:18:42 +0000

    we have a query working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,6 +6,7 @@ from lib.db import pool, query_wrap_array
 
 class HomeActivities:
   def run(cognito_user_id=None):
+    print("====home activities")
     # logger.info("HomeActivities")
     #with tracer.start_as_current_span("home-activities-mock-data"):
     #  span = trace.get_current_span()
@@ -13,8 +14,24 @@ class HomeActivities:
     #  span.set_attribute("app.now", now.isoformat())
 
     sql = query_wrap_array("""
-    SELECT * FROM activities
+    SELECT
+      activities.uuid,
+      users.display_name,
+      users.handle,
+      activities.message,
+      activities.replies_count,
+      activities.reposts_count,
+      activities.likes_count,
+      activities.reply_to_activity_uuid,
+      activities.expires_at,
+      activities.created_at
+    FROM public.activities
+    LEFT JOIN public.users ON users.uuid = activities.user_uuid
+    ORDER BY activities.created_at DESC
     """)
+    print("SQL------------")
+    print(sql)
+    print("SQL------------")
     with pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(sql)
