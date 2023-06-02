b48278dcd987f47d5f1cba361458678949d3148a
KingWhales <olawaleafosi@gmail.com>
2023-06-02 13:37:06 +0000

    ddb/seed updated

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -65,7 +65,7 @@ export default function MessageGroupPage() {
 
     loadMessageGroupsData();
     loadMessageGroupData();
-    checkAuth();
+    checkAuth(setUser);
   }, [])
   return (
     <article>
