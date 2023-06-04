aa6c6c7a2ce26971a42fb71e1eb85e563fc7de28
KingWhales <olawaleafosi@gmail.com>
2023-06-04 14:58:18 +0000

    getting londo to appear in message

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 from flask_awscognito import AWSCognitoAuthentication
-
+from services.users_short import *
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -278,6 +278,11 @@ def data_activities_reply(activity_uuid):
   else:
     return model['data'], 200
   return
+
+@app.route("/api/users/@<string:handle>/short", methods=['GET'])
+def data_users_short(handle):
+  data = UsersShort.run(handle)
+  return data, 200
 
 if __name__ == "__main__":
   app.run(debug=True)
