aa28bc812ae95ed35d6b08c299863215689255e7
KingWhales <olawaleafosi@gmail.com>
2023-06-05 21:09:24 +0000

    implemment dynamodb to our aapplication

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -112,7 +112,7 @@ class Ddb:
 
   def create_message_group(client, message,my_user_uuid, my_user_display_name, my_user_handle, other_user_uuid, other_user_display_name, other_user_handle):
     print('== create_message_group.1')
-    table_name = 'cruddur-messages'
+    table_name = 'cruddur-message'
 
     message_group_uuid = str(uuid.uuid4())
     message_uuid = str(uuid.uuid4())
